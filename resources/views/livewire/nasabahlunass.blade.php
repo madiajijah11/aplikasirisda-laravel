819c876929199ec0b010e0a5fe6c7c237b28a107
Dian Rahmadani <madiajijah7@gmail.com>
2021-03-11 13:42:46 +0800

    Update nasabahlunass.blade.php

--- resources/views/livewire/nasabahlunass.blade.php
+++ resources/views/livewire/nasabahlunass.blade.php
@@ -17,8 +17,7 @@
                     </div>
                 </div>
                 @endif
-                <button wire:click="create()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Tambah
-                    Nasabah</button>
+                <button wire:click="create()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Tambah Nasabah</button>
                 @if ($isModal)
                 @include('livewire.create-nasabahlunass')
                 @endif
